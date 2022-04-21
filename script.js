2b3aa286a44e94513932587341cd84709c392435
mibrahim234 <mohamm1417@gmail.com>
2022-04-21 15:44:24 -0400

    created constructor for calculator

--- script.js
+++ script.js
@@ -1,3 +1,38 @@
+// Store all the info with calculator class 
+// Constructor: takes all the inputs and functions for our calculator 
+class Calculator {
+    constructor(previousOperandTextElement, currentOperandTextElement) {
+        this.previousOperandTextElement = previousOperandTextElement
+        this.currentOperandTextElement = currentOperandTextElement
+    }
+    clear() {
+        this.currentOperand = ''
+        this.previousOperand = ''
+        this.operation = undefined
+    }
+
+    delete() {
+
+    }
+
+    appendNumber(number) {
+
+    }
+
+    chooseOperation(operation) {
+
+    }
+
+    compute() {
+
+    }
+
+    updateDisplay() {
+
+    }
+}
+
+
 // selecting the data attributes with brackets 
 // for single element we use regular querySelector 
 const numberButtons = document.querySelectorAll('[data-number]')
@@ -7,3 +42,4 @@ const deleteButton = document.querySelector('[data-delete]')
 const allClearButton = document.querySelector('[data-all-clear]')
 const previousOperandTextElement = document.querySelector('[data-previous-operand]')
 const currentOperandTextElement = document.querySelector('[data-current-operand]')
+
