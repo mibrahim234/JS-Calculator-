9101cabbbcf40ffb946c8d42184cbf221303198d
mibrahim234 <mohamm1417@gmail.com>
2022-04-21 15:39:20 -0400

    JS foundation

---
+++ script.js
@@ -0,0 +1,9 @@
+// selecting the data attributes with brackets 
+// for single element we use regular querySelector 
+const numberButtons = document.querySelectorAll('[data-number]')
+const operationButtons = document.querySelectorAll('[data-operation]')
+const equalsButton = document.querySelector('[data-equals]')
+const deleteButton = document.querySelector('[data-delete]')
+const allClearButton = document.querySelector('[data-all-clear]')
+const previousOperandTextElement = document.querySelector('[data-previous-operand]')
+const currentOperandTextElement = document.querySelector('[data-current-operand]')
